ded3aeacf029476cea9a5b2c286bbb9e38c3c417
Rafael Dias <rdmeneze@gmail.com>
2014-01-12 20:41:15 -0200

    - atualização do módulo utils.h

--- inc/utils.h
+++ inc/utils.h
@@ -1,18 +1,6 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
-#include "defs.h"
+#define VECTOR_LEN(x) (sizeof(x)/sizeof(x[0]))
 
-__inline WORD MAKEWORD( BYTE bLow, BYTE bHigh )
-{
-	return (bHigh << 8) + bLow;
-}
-
-__inline DWORD MAKEDWORD( WORD wLow, WORD wHigh )
-{
-	return (wHigh << 16) + wLow;
-}
-
-#define GET_ARRAY_LEN( x ) (sizeof(x)/sizeof(x[0]))
-
-#endif 
+#endif  // ~ __UTILS_H__
