206c0890b4ae58e77112964893bb69830c5b20e3
Rafael Dias <rdmeneze@gmail.com>
2014-01-12 20:45:05 -0200

    - atualizção do main

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
-#include "defs.h"
-#include "processor.h"
-#include "timer.h"
+/**
+ *	@file 	main.c
+ *	@brief	main file of SMSBoxV2.0 applicarion
+ *	@author	Rafael Das Menezes
+ *	@date 	jul/2013
+ */
+ 
+#include <processor.h>
 
-int main( void )
+
+int main( int argv, char** argc )
 {
-	
+
 }
+
