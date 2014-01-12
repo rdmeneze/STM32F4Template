bdb02fd348ed9c2f952a9d536f04889ed2100602
Rafael Dias <rdmeneze@gmail.com>
2014-01-12 20:40:00 -0200

    - adição do módulo unique count

---
+++ inc/uniquecount.h
@@ -0,0 +1,14 @@
+#ifndef __UNIQUECOUNT__
+#define __UNIQUECOUNT__
+
+#include <processor.h>
+
+
+/**
+ *	@brief	get the value of unique count of system
+ *	@return	value of the count
+ */
+uint32_t GetUniqueCount( void );
+
+
+#endif //~__UNIQUECOUNT__
---
+++ src/uniquecount.c
@@ -0,0 +1,16 @@
+#include <uniquecount.h>
+
+static uint32_t	dwUniqueCount = 0;
+
+
+int __svc(0) incUniqueCount (void);
+int __SVC_0      (void) {
+  return (dwUniqueCount++);
+}
+ 
+
+uint32_t GetUniqueCount( void )
+{
+	return incUniqueCount();
+}
+
