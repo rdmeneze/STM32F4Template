0c32b103b339f2746508de973f05ea2f2e6b351f
Rafael Dias <rdmeneze@gmail.com>
2014-02-28 09:55:50 -0300

    - some developments on the watchdog library

--- src/watchdog.c
+++ src/watchdog.c
@@ -1,25 +1,50 @@
 /**
- *  @brief  
+ *  @brief  IMplementation of WatchDog functionalities
+ *  @Author Rafael Dias Menezes <rdmeneze@gmail.com>
+ *  @date   December/2013
  */
 
 #include "errno.h"
 #include "watchdog.h"
 #include <string.h>
+#include "utils.h"
 #include <stm32f4xx_wwdg.h>
+#include <uniquecount.h>
 
 //----------------------------------------------------------------------------
 
-callbackwdt_func callback_watchdogtimer = NULL;
+#define WDT_ENTRY_CB    1
+#define WATCHDOG_MAGIC  0xDDAACCAA
+
+//----------------------------------------------------------------------------
+
+typedef struct _STWATCHDOGCB
+{
+	int32_t         magic;						//! magic number
+	void*           lpParam;					//! param passed in register function
+  uint32_t        handle;           
+	callbackwdt_func  callback_func;		//! callback function address
+}STWatchDogCB;
+
+STWatchDogCB vecWatchDogCB[WDT_ENTRY_CB];
 
 //----------------------------------------------------------------------------
 
 uint32_t	WatchDogInit( void )
 {
-	WWDG_SetPrescaler(WWDG_Prescaler_8);
-	WWDG_SetWindowValue( 0x45 );
-	WWDG_SetCounter( 0x7F );
-	WWDG_Enable( 0x7F );
+  int i;
+  STWatchDogCB * ptrSTWatchDogCB;
+  for ( i = 0, ptrSTWatchDogCB = &vecWatchDogCB[0]; i < VECTOR_LEN( vecWatchDogCB ); i++ )
+  {
+    ptrSTWatchDogCB->magic          = WATCHDOG_MAGIC;
+    ptrSTWatchDogCB->lpParam        = NULL;
+    ptrSTWatchDogCB->callback_func  = NULL;
+    ptrSTWatchDogCB->handle         = (uint32_t)-1;
+  }
 
+	WWDG_SetPrescaler(WWDG_Prescaler_8);
+	WWDG_SetWindowValue( 0x7F );
+	WWDG_Enable( 0x7F );
 
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_WWDG, ENABLE);
 	return 0;
@@ -34,10 +59,27 @@ uint32_t	WatchDogReset( void )
 }
 
 //----------------------------------------------------------------------------
-
-uint32_t	WatchDogRegisterCallBack( callbackwdt_func cb )
+uint32_t	WatchDogRegisterCallBack( callbackwdt_func cb, void* lParam, uint32_t* cbHandle )
 {
-	callback_watchdogtimer = cb;
+  if ( vecWatchDogCB[0].magic != WATCHDOG_MAGIC )
+  {
+    return EPERM;
+  }
+  
+  if ( cb == NULL )
+  {
+    return EPERM;
+  }
+
+  vecWatchDogCB[0].handle         = GetUniqueCount();
+  vecWatchDogCB[0].callback_func  = cb;
+  vecWatchDogCB[0].lpParam        = lParam;
+  
+  if ( cbHandle != NULL )
+{
+    *cbHandle = vecWatchDogCB[0].handle;
+  }
+  
 	WWDG_EnableIT();
 	return 0;
 }
@@ -46,10 +88,8 @@ uint32_t	WatchDogRegisterCallBack( callbackwdt_func cb )
 
 void WWDG_IRQHandler( void )    //! interrupt function
 {
-	if ( callback_watchdogtimer != NULL )
-	{
-		callback_watchdogtimer( 0 );
-	}
+  vecWatchDogCB[0].callback_func( vecWatchDogCB[0].lpParam );
+  
 	return;
 }
 
